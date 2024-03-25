17f4dc126f617c97101aa2f26c1619c8400d6619
Mercy-Chemtai <chemtailodite@gmail.com>
2024-03-25 08:32:37 +0300

    added changes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+    numbers(20,30,40)
+    words()
+    var sum = 20 + 30 + 40
+    println(sum)
+    var average = sum/3
+    println(average)
+    sphere()
+
+
+
+}
+fun  words():Array<String>{
+    var words = ("Barnie,bakes,brown,bagels,buns")
+    println(words)
+
+}
+
+fun numbers (num1:Int,num2:Int,num3:Int):Int {
+        return (num1 + num2 +num3)
+
+
+}
+   fun  sphere(){
+       var calculate = 3 * 3.14159 * (10*10*10)
+       var volume = 4/calculate
+       println(volume)
+
+   }
+fun isPalindrome(word:String):Boolean{
+        if( )
+}
+
